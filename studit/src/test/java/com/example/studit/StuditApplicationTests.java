5bd8208435631062b43dbb490733e80d148d0059
minaamim <minaaa5814@gmail.com>
2022-04-03 19:12:53 +0900

    Set initial configuration

---
+++ studit/src/test/java/com/example/studit/StuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
