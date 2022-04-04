13b60fa442eb9e543f02ec7b445933eb79473a64
minaamim <minaaa5814@gmail.com>
2022-04-04 15:33:17 +0900

    유저 엔티티
    성별은 남성/여성만 지정 가능하게

---
+++ studit/src/main/java/com/example/studit/domain/User.java
@@ -0,0 +1,39 @@
+package com.example.studit.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String phone;
+
+    @Column(nullable = false)
+    private String pwd;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    private Gender gender;
+
+    private String birth;
+
+}
