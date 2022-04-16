eff18cacaf4d0a01fd1efea60408743be272300c
minaamim <minaaa5814@gmail.com>
2022-04-16 12:21:20 +0900

    add: add jwt token filter

--- studit/src/main/java/com/example/studit/config/WebSecurityConfig.java
+++ studit/src/main/java/com/example/studit/config/WebSecurityConfig.java
@@ -1,25 +1,102 @@
 package com.example.studit.config;
 
+import com.example.studit.security.JwtAuthenticationFilter;
+import com.example.studit.security.JwtTokenProvider;
+import com.example.studit.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@AllArgsConstructor
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    //비밀번호 암호화를 위한 Bean
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+
+    // 정적인 파일에 대한 요청들
+    private static final String[] AUTH_WHITELIST = {
+            // -- swagger ui
+            "/v2/api-docs",
+            "/v3/api-docs/**",
+            "/configuration/ui",
+            "/swagger-resources/**",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            "/file/**",
+            "/image/**",
+            "/swagger/**",
+            "/swagger-ui/**",
+            // other public endpoints of your API may be appended to this array
+            "/h2/**"
+    };
+
+    /*
+    web FilterChainProxy 생성 필터
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        //Spring Security 가 인증을 무시할 경로 설정
+//        web.ignoring().antMatchers("/css/**", "/img/**", "/js/**", "/lib/**", "/vendor/**");
+        web.ignoring().antMatchers(AUTH_WHITELIST);
+    }
+
+
+    /*
+    Security 설정
+    @param http HTTP 요청에 대한 보안 구성
+    @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception{
+//        http.cors().disable()
+//                .cors().disable() //cors 방지
+//                .csrf().disable() //csrf 방지
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .formLogin().disable() //기본 로그인 페이지 없애기
+//                .headers().frameOptions().disable()
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/admin/**").hasRole("ADMIN") //ADMIN만 접근 허용
+//                .antMatchers("/auth/**").hasRole("USER") //USER만 접근 허용
+//                .antMatchers("/**").permitAll();
+
         http.cors().disable()
-                .cors().disable() //cors 방지
-                .csrf().disable() //csrf 방지
-                .formLogin().disable() //기본 로그인 페이지 없애기
-                .headers().frameOptions().disable();
+                .cors().disable()
+                .csrf().disable().authorizeRequests()
+                //login 없이 접근 허용 하는 url
+                .antMatchers("/user/**").permitAll()
+                //admin의 경우 ADMIN 권한이 있는 사용자만 접근 가능
+                .antMatchers("/admin").hasRole("ADMIN")
+                //그 외 모든 요청은 인증과정 필요
+                .anyRequest().authenticated()
+                .and()
+                //토큰 기반 인증이라 session 사용 x
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                //JwtAuthenticationFilter는 UsernamePasswordAuthenticationFilter 전에 넣음
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
     }
 }
