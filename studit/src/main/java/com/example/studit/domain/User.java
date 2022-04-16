849ec10e6164cc6fdf46d83e8d4ea583e22e67f1
minaamim <minaaa5814@gmail.com>
2022-04-16 12:17:29 +0900

    feat: create member entity, member repository and role enum type

--- studit/src/main/java/com/example/studit/domain/User.java
+++ studit/src/main/java/com/example/studit/domain/User.java
@@ -1,14 +1,21 @@
 package com.example.studit.domain;
 
 import lombok.*;
+import net.bytebuddy.build.Plugin;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @Table(name = "user")
-public class User {
+public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -33,11 +40,19 @@ public class User {
 
     private String birth;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     @Builder
     public User(String userName, String phone, String pwd, String email){
         this.userName = userName;
         this.phone = phone;
         this.pwd = pwd;
         this.email = email;
+        this.role = Role.USER;
+    }
+
+    public void encryptPassword(PasswordEncoder passwordEncoder) {
+        pwd = passwordEncoder.encode(pwd);
     }
 }
