d10e8b19cf47ab0a56cbc80c9f30043486d5c600
minaamim <minaaa5814@gmail.com>
2022-04-22 17:24:31 +0900

    feat: Create Challenge-Related Entities&Repository
    챌린지 관련 엔티티 및 레포지 생성

---
+++ studit/src/main/java/com/example/studit/domain/challenge/ParticipatedChallenge.java
@@ -0,0 +1,26 @@
+package com.example.studit.domain.challenge;
+
+import com.example.studit.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ParticipatedChallenge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "participated_challenge_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_id")
+    private Challenge challenge;
+}
