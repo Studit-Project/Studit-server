8612f4d1cdabb8aab7050e4a1f856a7bca5945c1
minaamim <minaaa5814@gmail.com>
2022-04-23 20:48:49 +0900

    add: Add extension

--- studit/src/main/java/com/example/studit/repository/ChallengeRepository.java
+++ studit/src/main/java/com/example/studit/repository/ChallengeRepository.java
@@ -1,4 +1,9 @@
 package com.example.studit.repository;
 
-public interface ChallengeRepository {
+import com.example.studit.domain.challenge.Challenge;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 }
