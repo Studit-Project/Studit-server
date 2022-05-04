61bf52f103fe8cec3aaf0cace4ac4f0c98586903
minaamim <minaaa5814@gmail.com>
2022-05-04 18:08:35 +0900

    add: Add badge-related entities

---
+++ studit/src/main/java/com/example/studit/domain/MyBadge.java
@@ -0,0 +1,28 @@
+package com.example.studit.domain;
+
+import com.example.studit.domain.Badge;
+import com.example.studit.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class MyBadge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "my_badge_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "badge_id")
+    private Badge badge;
+}
