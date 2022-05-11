dca5280bfb51f8bc8c9b5807c0709933e5e0a770
minaamim <minaaa5814@gmail.com>
2022-05-11 17:01:10 +0900

    feat: Import individual study list

--- studit/src/main/java/com/example/studit/controller/StudyController.java
+++ studit/src/main/java/com/example/studit/controller/StudyController.java
@@ -3,14 +3,18 @@ package com.example.studit.controller;
 import com.example.studit.domain.User;
 import com.example.studit.domain.study.Study;
 import com.example.studit.dto.StudyCreateDto;
+import com.example.studit.dto.StudyManageDto;
 import com.example.studit.service.StudyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,4 +29,9 @@ public class StudyController {
         return studyService.save(studyCreateDto);
     }
 
+    //스터디 관리
+    @GetMapping("/study/manage")
+    public List<StudyManageDto> StudyManage(){
+        return studyService.findIndividualStudies();
+    }
 }
--- studit/src/main/java/com/example/studit/repository/StudyRepository.java
+++ studit/src/main/java/com/example/studit/repository/StudyRepository.java
@@ -1,11 +1,33 @@
 package com.example.studit.repository;
 
-import com.example.studit.domain.User;
 import com.example.studit.domain.study.MyStudy;
+import com.example.studit.domain.study.ParticipatedStudy;
 import com.example.studit.domain.study.Study;
+import com.example.studit.dto.StudyManageDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Long> {
+
+    Collection<? extends Study> findAllByLeaderIdIn(Set<Long> Leader);
+
+
+    Collection<? extends Study> findAllByParticipatedMembersIdIn(Set<Long> participatedMembers);
+
+//    Collection<? extends Study> findAllByLeader(Set<MyStudy> myStudySet);
+//
+//    Collection<? extends Study> findAllByParticipatedMembers(Set<ParticipatedStudy> participatedStudySet);
+
+//    Collection<? extends Study> findByLeader(Set<MyStudy> myStudySet);
+//
+//    Collection<? extends Study> findByParticipatedMembers(Set<ParticipatedStudy> participatedStudySet);
+
+//    Collection<? extends Study> findAllByMyStudy(List<MyStudy> myStudies);
 }
