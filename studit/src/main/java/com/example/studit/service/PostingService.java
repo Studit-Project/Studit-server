569873e07c25a6deff19c58f9ddcccb85d69b2a8
minaamim <minaaa5814@gmail.com>
2022-05-18 20:34:56 +0900

    refact: Update postingService
    - return postingDto -> postingListDto
    - create feature to return a posting

--- studit/src/main/java/com/example/studit/service/PostingService.java
+++ studit/src/main/java/com/example/studit/service/PostingService.java
@@ -3,8 +3,12 @@ package com.example.studit.service;
 import com.example.studit.domain.Category;
 import com.example.studit.domain.Posting;
 import com.example.studit.domain.User;
+import com.example.studit.dto.CommentResponseDto;
 import com.example.studit.dto.PostingDto;
+import com.example.studit.dto.PostingListDto;
 import com.example.studit.dto.UserInfoDto;
+import com.example.studit.mapper.CommentMapper;
+import com.example.studit.mapper.PostingListMapper;
 import com.example.studit.mapper.PostingMapper;
 import com.example.studit.repository.PostingRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -24,6 +30,10 @@ public class PostingService {
     private final UserService userService;
     @Autowired
     private final PostingMapper postingMapper;
+    @Autowired
+    private final CommentMapper commentMapper;
+    @Autowired
+    private final PostingListMapper postingListMapper;
 
     public Long save(PostingDto postingDto){
 
@@ -32,12 +42,51 @@ public class PostingService {
 
         postingDto.setUserInfoDto(userInfoDto);
 
-        Posting posting = new Posting(postingDto.getCategory(), user, postingDto.getLocalDateTime(), postingDto.getContent());
+        Posting posting = new Posting(postingDto.getCategory(), postingDto.getTitle(), user, postingDto.getLocalDateTime(), postingDto.getContent());
         return postingRepository.save(posting).getId();
     }
 
-    public List<PostingDto> findAllStudyPosting(String category) {
+    public List<PostingListDto> findAllStudyPosting(String category) {
         Category categoryEnum = Category.valueOf(category);
-        return postingMapper.toDtoList(postingRepository.findAllByCategory(categoryEnum));
+
+        List<Posting> postingList = postingRepository.findAllByCategory(categoryEnum);
+
+        List<PostingListDto> list = new ArrayList<>();
+        for ( Posting posting : postingList ) {
+            list.add( PostingListDto.builder()
+                    .id(posting.getId())
+                    .title(posting.getTitle())
+                            .userId(posting.getUser().getId())
+                            .localDateTime(posting.getLocalDateTime()).build()
+                    );
+        }
+
+        return list;
+    }
+
+    public PostingDto readOne(Long postingId) {
+        Optional<Posting> posting = postingRepository.findById(postingId);
+
+        UserInfoDto userInfoDto = UserInfoDto.builder()
+                                .user(posting.get().getUser())
+                                .build();
+
+        List<CommentResponseDto> commentResponseDtoList = commentMapper.toDtoList(posting.get().getComments());
+
+
+
+        PostingDto postingDto = PostingDto.builder()
+                .id(posting.get().getId())
+                .userInfoDto(userInfoDto)
+                .category(posting.get().getCategory())
+                .commentList(commentResponseDtoList)
+                .localDateTime(posting.get().getLocalDateTime())
+                .content(posting.get().getContent())
+                .build();
+
+        return postingDto;
+
+
+//        return postingMapper.toDto(posting.orElseThrow());
     }
 }
