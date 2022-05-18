4598dd0a80714fa4c512faf67bd12ca01e04fc98
minaamim <minaaa5814@gmail.com>
2022-05-18 20:32:29 +0900

    refact: Modify return value
    postingDto -> postingListDto

--- studit/src/main/java/com/example/studit/controller/PostingController.java
+++ studit/src/main/java/com/example/studit/controller/PostingController.java
@@ -3,15 +3,13 @@ package com.example.studit.controller;
 import com.example.studit.domain.Posting;
 import com.example.studit.domain.User;
 import com.example.studit.dto.PostingDto;
+import com.example.studit.dto.PostingListDto;
 import com.example.studit.dto.UserInfoDto;
 import com.example.studit.service.PostingService;
 import com.example.studit.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,9 +28,16 @@ public class PostingController {
         return postingService.save(postingDto);
     }
 
+    /*스터디 전체 글 보기*/
     @GetMapping("/posting")
-    public List<PostingDto> studyPostings(){
+    public List<PostingListDto> studyPostings(){
         return postingService.findAllStudyPosting("STUDY");
+    }
+
+    /*글 상세 보기*/
+    @GetMapping("/posting/{postingId}")
+    public PostingDto read(@PathVariable("postingId") Long postingId){
+        return postingService.readOne(postingId);
     }
 
 }
