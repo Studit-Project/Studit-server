a7a4f80eac90255f08b7e3a2113167524c952646
minaamim <minaaa5814@gmail.com>
2022-05-18 20:29:35 +0900

    add: Add comment
    - comment: builder 추가, 연관관계 메서드 추가
    - commentRequestDto: 생성 시 필요한 dto
    - commentResponseDto: 나타낼 때 필요한 dto

---
+++ studit/src/main/java/com/example/studit/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.example.studit.repository;
+
+import com.example.studit.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
