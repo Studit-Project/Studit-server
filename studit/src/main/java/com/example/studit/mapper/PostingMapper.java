ee73c5354e4729489c02ee761ff446754a9bb638
minaamim <minaaa5814@gmail.com>
2022-05-24 18:59:00 +0900

    add: Delete mapper
    mapper 전체 삭제, 직접 변환

--- studit/src/main/java/com/example/studit/mapper/PostingMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.studit.mapper;
-
-import com.example.studit.domain.Posting;
-import com.example.studit.dto.PostingDto;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface PostingMapper extends GenericMapper<PostingDto, Posting> {
-}
--- studit/src/main/java/com/example/studit/mapper/StudyMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.studit.mapper;
-
-import com.example.studit.domain.study.Study;
-import com.example.studit.dto.StudyManageDto;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface StudyMapper extends GenericMapper<StudyManageDto, Study>{
-}
