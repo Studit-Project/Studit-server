fa0f4605950e72ea7d69b9e2fc0c7a50a013d837
minaamim <minaaa5814@gmail.com>
2022-05-25 16:15:58 +0900

    add: Update studyService
    - delete setter
    - create an function to add a study member

--- studit/src/main/java/com/example/studit/service/StudyService.java
+++ studit/src/main/java/com/example/studit/service/StudyService.java
@@ -6,12 +6,10 @@ import com.example.studit.domain.study.ParticipatedStudy;
 import com.example.studit.domain.study.Study;
 import com.example.studit.dto.StudyCreateDto;
 import com.example.studit.dto.StudyManageDto;
-import com.example.studit.mapper.StudyMapper;
 import com.example.studit.repository.MyStudyRepository;
 import com.example.studit.repository.ParticipatedStudyRepository;
 import com.example.studit.repository.StudyRepository;
 import com.example.studit.repository.UserRepository;
-import com.google.inject.internal.util.Sets;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -20,11 +18,8 @@ import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.http.Part;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -46,10 +41,7 @@ public class StudyService {
     @Autowired
     private final UserService userService;
 
-    @Autowired
-    private final StudyMapper studyMapper;
-
-    //스터디룸 개설
+    /*스터디룸 개설*/
     public Long save(StudyCreateDto studyCreateDto){
 
         //현재 로그인한 유저 정보
@@ -58,20 +50,17 @@ public class StudyService {
         User user = userRepository.findByUserName(authentication.getName());
 
         MyStudy myStudy = new MyStudy();
-        myStudy.setUser(user);
+        myStudy.addUser(user);
 
         Study study = studyRepository.save(studyCreateDto.toEntity());
 
         myStudyRepository.save(myStudy);
-        study.setUser(myStudy);
+        study.addLeader(myStudy);
 
         return study.getId();
     }
 
-//    public List<StudyManageDto> findByPhone(String phone) {
-//        return studyRepository.findByPhone(phone);
-//    }
-
+    /*개인이 참여한 스터디 목록*/
     public List<StudyManageDto> findIndividualStudies() {
         User user = userService.getUserFromAuth();
 
@@ -90,17 +79,37 @@ public class StudyService {
         }
 
         if(!participatedStudies.isEmpty()){
-            for(int i = 0; i < myStudies.size(); i++)
+            for(int i = 0; i <= myStudies.size(); i++)
                 participatedStudyId = Set.of(participatedStudies.get(i).getId());
         }
 
         studies.addAll(studyRepository.findAllByLeaderIdIn(myStudyId));
         studies.addAll(studyRepository.findAllByParticipatedMembersIdIn(participatedStudyId));
 
+        List<StudyManageDto> studyManageDtos = studies.stream()
+                .map(StudyManageDto::new)
+                .collect(Collectors.toList());
 
-
-        return studyMapper.toDtoList(studies);
-
-//        return studies;
+        return studyManageDtos;
     }
+
+    /*스터디원 추가*/
+    public Long addStudyOne(Long studyId, String nickname) {
+        User user = userService.getUserFromAuth();
+        Optional<Study> study = studyRepository.findById(studyId);
+        User leader = study.get().getLeader().getUser();
+        ParticipatedStudy participatedStudy = new ParticipatedStudy();
+
+        if(user == leader){
+            //참여한 스터디 추가
+            participatedStudy.addUser(userRepository.findUserByNickname(nickname));
+            participatedStudyRepository.save(participatedStudy);
+
+            //스터디에 추가
+            study.get().addOne(participatedStudy);
+        }
+
+        return participatedStudy.getId();
+    }
+
 }
