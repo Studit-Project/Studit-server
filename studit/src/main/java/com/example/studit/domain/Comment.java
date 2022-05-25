4e89f1fa96321b61c1ac35cb5c67e7daa36e103d
minaamim <minaaa5814@gmail.com>
2022-05-26 02:34:16 +0900

    add: Add createdDate

--- studit/src/main/java/com/example/studit/domain/Comment.java
+++ studit/src/main/java/com/example/studit/domain/Comment.java
@@ -1,9 +1,11 @@
 package com.example.studit.domain;
 
+import com.example.studit.domain.User.User;
 import com.example.studit.domain.posting.Posting;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -21,6 +23,7 @@ public class Comment extends BaseEntity{
     @JoinColumn(name = "user_id")
     private User user;
 
+    @CreatedDate
     private LocalDateTime localDateTime = LocalDateTime.now();
 
     @ManyToOne(fetch = FetchType.LAZY)
