46779335000564eafd71f6b3d8cd01f8db9e37f3
minaamim <minaaa5814@gmail.com>
2022-05-26 02:36:00 +0900

    add: Create PatchDetailReq
    - dto for user details

---
+++ studit/src/main/java/com/example/studit/domain/User/dto/PatchDetailReq.java
@@ -0,0 +1,15 @@
+package com.example.studit.domain.User.dto;
+
+import com.example.studit.domain.enumType.Gender;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class PatchDetailReq {
+    private String nickname;
+    private Gender gender;
+    private LocalDate birth;
+}
