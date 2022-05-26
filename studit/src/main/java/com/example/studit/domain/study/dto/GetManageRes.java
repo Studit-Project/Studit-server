c35286232881fd8cc21533f6aec5cac59c04bfed
minaamim <minaaa5814@gmail.com>
2022-05-26 15:14:37 +0900

    add: Update Study: GetManageRest dto
    - Delete introduction
    - Add constructor

--- studit/src/main/java/com/example/studit/domain/study/dto/GetManageRes.java
+++ studit/src/main/java/com/example/studit/domain/study/dto/GetManageRes.java
@@ -1,6 +1,8 @@
 package com.example.studit.domain.study.dto;
 
 import com.example.studit.domain.study.Activity;
+import com.example.studit.domain.study.MyStudy;
+import com.example.studit.domain.study.ParticipatedStudy;
 import com.example.studit.domain.study.Study;
 import com.example.studit.dto.UserInfoDto;
 import lombok.*;
@@ -19,8 +21,6 @@ public class GetManageRes {
     //스터디명
     private String name;
 
-    private String introduction;
-
     private int number;
 
     private int currentNum;
@@ -29,12 +29,11 @@ public class GetManageRes {
 
     private Activity activity;
 
-    private List<UserInfoDto> participatedMembers = new ArrayList<>();
+    private List<UserInfoDto> participatedMembers;
 
     public GetManageRes(Study study){
         this.id = study.getId();
         this.name = study.getName();
-        this.introduction = study.getIntroduction();
         this.number = study.getNumber();
         this.currentNum = study.getParticipatedMembers().size() + 1;
         this.activity = study.getActivity();
@@ -43,16 +42,25 @@ public class GetManageRes {
                 .collect(Collectors.toList());
     }
 
-//    @Builder
-//    public StudyManageDto( String name, String introduction, int number, int currentNum, UserInfoDto leader, Activity activity, List<UserInfoDto> participatedMembers){
-////        this.id = id;
-//        this.name = name;
-//        this.introduction = introduction;
-//        this.number = number;
-//        this.currentNum = currentNum;
-//        this.leader = leader;
-//        this.participatedMembers = participatedMembers;
-//    }
+    public GetManageRes(MyStudy myStudy){
+        this.id = myStudy.getStudy().getId();
+        this.name = myStudy.getStudy().getName();
+        this.number = myStudy.getStudy().getNumber();
+        this.currentNum = myStudy.getStudy().getParticipatedMembers().size() + 1;
+        this.activity = myStudy.getStudy().getActivity();
+        this.participatedMembers = myStudy.getStudy().getParticipatedMembers()
+                .stream().map(UserInfoDto::new)
+                .collect(Collectors.toList());
+    }
 
+    public GetManageRes(ParticipatedStudy participatedStudy){
+        this.id = participatedStudy.getStudy().getId();
+        this.name = participatedStudy.getStudy().getName();
+        this.number = participatedStudy.getStudy().getParticipatedMembers().size() + 1;
+        this.activity = participatedStudy.getStudy().getActivity();
+        this.participatedMembers = participatedStudy.getStudy().getParticipatedMembers()
+                .stream().map(UserInfoDto::new)
+                .collect(Collectors.toList());
+    }
 
 }
