fc9b83315623b370cbdf3a74736c781000895aa4
minaamim <minaaa5814@gmail.com>
2022-05-29 20:20:11 +0900

    refactor: Delete mapper
    저번에 삭제했는데 왜 남아있지!?

--- studit/src/main/java/com/example/studit/mapper/CommentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.studit.mapper;
-
-import com.example.studit.domain.Comment;
-import com.example.studit.dto.CommentResponseDto;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface CommentMapper extends GenericMapper<CommentResponseDto, Comment> {
-}
--- studit/src/main/java/com/example/studit/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.studit.mapper;
-
-import com.example.studit.domain.User;
-import com.example.studit.dto.UserInfoDto;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface UserMapper extends GenericMapper <UserInfoDto, User> {
-}
