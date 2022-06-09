30cfc67709a8bcca49524a92539a913dc6cc3773
minaamim <minaaa5814@gmail.com>
2022-06-09 18:26:42 +0900

    add: Add Likes Repository

---
+++ studit/src/main/java/com/example/studit/repository/LikesRepository.java
@@ -0,0 +1,9 @@
+package com.example.studit.repository;
+
+import com.example.studit.domain.likes.Likes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LikesRepository extends JpaRepository<Likes, Long> {
+}
