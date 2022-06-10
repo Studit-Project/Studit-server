f4ad2e16f52442ecde97a9e745155234c89b91da
minaamim <minaaa5814@gmail.com>
2022-06-10 15:28:11 +0900

    add: Create Study Interior Response Dto

---
+++ studit/src/main/java/com/example/studit/domain/study/dto/GetInteriorRes.java
@@ -0,0 +1,29 @@
+package com.example.studit.domain.study.dto;
+
+import com.example.studit.domain.User.dto.UserInfoDto;
+import com.example.studit.domain.study.Study;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+public class GetInteriorRes {
+    private Long id;
+    private String name;
+    private String introduction;
+    private UserInfoDto leader;
+    private List<UserInfoDto> followers;
+
+    public GetInteriorRes(Study study){
+        this.id = study.getId();
+        this.name = study.getName();
+        this.introduction = study.getIntroduction();
+        this.leader = study.getLeader().getUser().toUserInfoDto();
+        this.followers = study.getParticipatedMembers().stream()
+                .map(UserInfoDto::new)
+                .collect(Collectors.toList());
+    }
+}
