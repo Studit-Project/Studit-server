b82321d29f923905369f603c16af98ced0aa3865
minaamim <minaaa5814@gmail.com>
2022-06-16 15:52:57 +0900

    feat: Creating a detailed view of posts
    - 스터디 내 게시물 상세 보기 기능

---
+++ studit/src/main/java/com/example/studit/domain/bulletin/dto/GetDetailRes.java
@@ -0,0 +1,34 @@
+package com.example.studit.domain.bulletin.dto;
+
+import com.example.studit.domain.User.dto.UserInfoDto;
+import com.example.studit.domain.bulletin.BulletinBoard;
+import com.example.studit.domain.comment.dto.CommentRequestDto;
+import com.example.studit.domain.comment.dto.CommentResponseDto;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+public class GetDetailRes {
+    private Long id;
+    private UserInfoDto userInfoDto;
+    private String title;
+    private LocalDateTime createAt;
+    private String content;
+    private List<CommentResponseDto> comments;
+
+    public GetDetailRes(BulletinBoard bulletinBoard){
+        this.id = bulletinBoard.getId();
+        this.userInfoDto = bulletinBoard.getUser().toUserInfoDto();
+        this.title = bulletinBoard.getTitle();
+        this.createAt = bulletinBoard.getCreateAt();
+        this.content = bulletinBoard.getContent();
+        this.comments = bulletinBoard.getComments().stream()
+                .map(CommentResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
