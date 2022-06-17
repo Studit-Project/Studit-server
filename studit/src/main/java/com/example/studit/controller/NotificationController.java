b1c71f294df4954e9360e7d1e00c26bea08b391f
minaamim <minaaa5814@gmail.com>
2022-06-18 01:25:18 +0900

    feat: Create Notification Function
    - 알림 기능 생성

---
+++ studit/src/main/java/com/example/studit/controller/NotificationController.java
@@ -0,0 +1,24 @@
+package com.example.studit.controller;
+
+import com.example.studit.config.swagger.BaseResponse;
+import com.example.studit.service.NotificationService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequiredArgsConstructor
+public class NotificationController {
+    private final NotificationService notificationService;
+
+    /** @title 로그인 한 유저 sse 연결 **/
+    @ApiOperation(value = "알림 구독", notes = "알림을 구독한다.")
+    @GetMapping(value = "/subscribe/{id}", produces = "text/event-stream")
+    public BaseResponse<SseEmitter> subscribe(@PathVariable Long id, @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId){
+        return new BaseResponse<SseEmitter>(notificationService.subscribe(id, lastEventId));
+    }
+}
---
+++ studit/src/main/java/com/example/studit/domain/notification/NotificationType.java
@@ -0,0 +1,5 @@
+package com.example.studit.domain.notification;
+
+public enum NotificationType {
+    COMMENT, ACCEPT, REFUSE, INVITE, EXPEL, LIKES
+}
