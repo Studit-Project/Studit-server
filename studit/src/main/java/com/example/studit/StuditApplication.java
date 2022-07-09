fb0e8b69fb7747cbbaba82aa8b24cc74c6f5cb25
minaamim <minaaa5814@gmail.com>
2022-07-09 15:42:40 +0900

    add: Add s3 settings
    - yml 설정 추가
    - s3 설정 추가

--- studit/src/main/java/com/example/studit/StuditApplication.java
+++ studit/src/main/java/com/example/studit/StuditApplication.java
@@ -2,14 +2,23 @@ package com.example.studit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class StuditApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:aws.yml";
 	public static void main(String[] args) {
-		SpringApplication.run(StuditApplication.class, args);
+
+		new SpringApplicationBuilder(StuditApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+
+//		SpringApplication.run(StuditApplication.class, args);
 	}
 
 }
