2b0e25f8cbcf506177d5a3409631ff8f2c6cef2e
minaamim <minaaa5814@gmail.com>
2022-07-23 17:19:24 +0900

    chore: Update yml
    - Modify jwt details
    - Add generate-ddl

---
+++ studit/src/main/java/com/example/studit/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.studit.controller;
+
+import com.example.studit.security.JwtTokenProvider;
+import com.example.studit.service.auth.AuthService;
+import com.example.studit.service.auth.KakaoAuthService;
+import com.example.studit.service.auth.NaverAuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class AuthController {
+//    private final KakaoAuthService kakaoAuthService;
+//    private final NaverAuthService naverAuthservice;
+//    private final JwtTokenProvider jwtTokenProvider;
+//    private final AuthService authService;
+
+    /**
+     * KAKAO 소셜 로그인 기능
+     **/
+//    @ApiOperation(value = "카카오 로그인", notes = "카카오 액세스 토큰을 이용해 사용자 정보 받아 저장하고 앱의 토큰 신환")
+//    @PostMapping(value = "/kakao")
+//    public BaseResponse<AuthResponse> kakaoAuthRequest(@RequestBody AuthRequest authRequest) {
+//
+//    }
+
+}
