a0f45d20a0accc8e5f382a347c222c632e18a9fb
minaamim <minaaa5814@gmail.com>
2022-08-17 01:52:14 +0900

    add: Update GetProfileDto
    - 프로필에 노출되는 포스팅 정보 수정

--- studit/src/main/java/com/example/studit/domain/posting/dto/GetProfileDto.java
+++ studit/src/main/java/com/example/studit/domain/posting/dto/GetProfileDto.java
@@ -2,22 +2,30 @@ package com.example.studit.domain.posting.dto;
 
 import com.example.studit.domain.enumType.Category;
 import com.example.studit.domain.posting.Posting;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class GetProfileDto {
     private Long id;
     private Category category;
+    private String title;
     private String content;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime cratedAt;
     private int likes;
     private int comments;
 
     public GetProfileDto(Posting posting){
         this.id = posting.getId();
         this.category = posting.getCategory();
+        this.title = posting.getTitle();
         this.content = posting.getContent();
+        this.cratedAt = posting.getUpdatedAt();
         this.likes = posting.getLikes().size();
         this.comments = posting.getComments().size();
     }
