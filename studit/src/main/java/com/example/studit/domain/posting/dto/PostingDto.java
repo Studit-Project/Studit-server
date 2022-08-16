df6c1f3000985de815e38eea865f24367f072498
minaamim <minaaa5814@gmail.com>
2022-08-16 14:50:53 +0900

    add: Update postingDto
    - Add province, activity, target, gender

--- studit/src/main/java/com/example/studit/domain/posting/dto/PostingDto.java
+++ studit/src/main/java/com/example/studit/domain/posting/dto/PostingDto.java
@@ -1,9 +1,13 @@
 package com.example.studit.domain.posting.dto;
 
 import com.example.studit.domain.comment.dto.CommentResponseDto;
+import com.example.studit.domain.enumType.Gender;
+import com.example.studit.domain.enumType.Target;
 import com.example.studit.domain.posting.Posting;
 import com.example.studit.domain.enumType.Category;
 import com.example.studit.domain.User.dto.UserInfoDto;
+import com.example.studit.domain.posting.Province;
+import com.example.studit.domain.study.Activity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
@@ -32,6 +36,11 @@ public class PostingDto {
 
     private String content;
 
+    private Province province;
+    private Activity activity;
+    private Target target;
+    private Gender gender;
+
     private List<CommentResponseDto> commentList;
 
     @Builder
@@ -42,6 +51,10 @@ public class PostingDto {
         this.userInfoDto = posting.getUser().toUserInfoDto();
         this.localDateTime = posting.getUpdatedAt();
         this.content = posting.getContent();
+        this.province = posting.getProvince();
+        this.activity = posting.getActivity();
+        this.target = posting.getTarget();
+        this.gender = posting.getGender();
         this.commentList = posting.getComments().stream()
                 .map(CommentResponseDto::new)
                 .collect(Collectors.toList());
