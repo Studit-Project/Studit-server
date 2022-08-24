8805d53327b490910be01d636539d294239ab575
minaamim <minaaa5814@gmail.com>
2022-08-24 14:30:21 +0900

    fix: Fix it to get all studies

--- studit/src/main/java/com/example/studit/service/HomeService.java
+++ studit/src/main/java/com/example/studit/service/HomeService.java
@@ -2,7 +2,9 @@ package com.example.studit.service;
 
 import com.example.studit.domain.User.User;
 import com.example.studit.domain.study.ParticipatedStudy;
+import com.example.studit.domain.study.Study;
 import com.example.studit.domain.study.dto.GetHomeStudiesRes;
+import com.example.studit.domain.study.dto.GetManageRes;
 import com.example.studit.dto.GetHomeRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,13 +25,23 @@ public class HomeService {
 
         //가장 진행률 높은 챌린지 하나 가져오기(예정)
 
-        /*스터디 리스트 가져오기*/
+        /** 스터디 리스트 가져오기 **/
+        //내가 리더
+        List<Study> studies = user.getStudies();
+        List<GetHomeStudiesRes> studiesRes = studies.stream()
+                .map(GetHomeStudiesRes::new)
+                .collect(Collectors.toList());
 
-        List<ParticipatedStudy> participatedStudies = user.getParticipatedStudies(); //내가 참여한 스터디 리스트
+        //내가 참여한 스터디 리스트
+        List<ParticipatedStudy> participatedStudies = user.getParticipatedStudies();
 
         List<GetHomeStudiesRes> getHomeStudiesRes = participatedStudies.stream()
                 .map(GetHomeStudiesRes::new)
                 .collect(Collectors.toList());
+
+        for(GetHomeStudiesRes getHomeStudiesRes1 : studiesRes) {
+            getHomeStudiesRes.add(getHomeStudiesRes1);
+        }
 
 
         GetHomeRes getHomeRes = new GetHomeRes(user, getHomeStudiesRes);
